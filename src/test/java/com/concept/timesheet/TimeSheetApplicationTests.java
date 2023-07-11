3ef8540cbe10475574a1867889e873791ffb9002
rohit <rohit.prakash@ideas2it.com>
2023-07-11 11:57:37 +0530

    first commit

---
+++ src/test/java/com/concept/timesheet/TimeSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concept.timesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
